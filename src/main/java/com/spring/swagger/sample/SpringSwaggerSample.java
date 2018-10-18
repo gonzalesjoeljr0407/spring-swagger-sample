2235f6b10cdce8606adeca01ba868debbdf47755
Joel Gonzales <gonzalesjoeljr0407@gmail.com>
2018-10-18 07:42:29 +0000

    basic controller with swagger

---
+++ src/main/java/com/spring/swagger/sample/SpringSwaggerSample.java
@@ -0,0 +1,12 @@
+package com.spring.swagger.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerSample {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSwaggerSample.class, args);
+    }
+}
---
+++ src/main/java/com/spring/swagger/sample/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.spring.swagger.sample.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secured")
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public ResponseEntity<String> returnHello() {
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+}
