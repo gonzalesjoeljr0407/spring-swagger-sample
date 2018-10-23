b41ca9cb7d477176e47b93064a68a16ab80979ad
Joel Gonzales <gonzalesjoeljr0407@gmail.com>
2018-10-23 06:43:20 +0000

    add credentials

---
+++ src/main/java/com/spring/swagger/sample/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.spring.swagger.sample.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+  
+}
